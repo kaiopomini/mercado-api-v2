257d594a716e3744cd07d25c48f1366e0a99988e
Kaio Pomini <kaiopomini@gmail.com>
2022-12-24 14:44:15 -0300

    chore(main): remove comments

--- src/main.ts
+++ src/main.ts
@@ -11,30 +11,27 @@ async function bootstrap() {
     .setTitle('Documentação - Mercado api')
     .setDescription('')
     .setVersion('1.0')
-
     .addBearerAuth(
       {
-        // I was also testing it without prefix 'Bearer ' before the JWT
         description: `[just text field] Please enter token in following format: Bearer <JWT>`,
         name: 'Authorization',
-        bearerFormat: 'Bearer', // I`ve tested not to use this field, but the result was the same
+        bearerFormat: 'Bearer',
         scheme: 'Bearer',
-        type: 'http', // I`ve attempted type: 'apiKey' too
+        type: 'http',
         in: 'Header',
       },
-      'access-token', // This name here is important for matching up with @ApiBearerAuth() in your controller!
+      'access-token',
     )
     .addBearerAuth(
       {
-        // I was also testing it without prefix 'Bearer ' before the JWT
         description: `[just text field] Please enter token in following format: Bearer <JWT>`,
         name: 'Authorization',
-        bearerFormat: 'Bearer', // I`ve tested not to use this field, but the result was the same
+        bearerFormat: 'Bearer',
         scheme: 'Bearer',
-        type: 'http', // I`ve attempted type: 'apiKey' too
+        type: 'http',
         in: 'Header',
       },
-      'refresh-token', // This name here is important for matching up with @ApiBearerAuth() in your controller!
+      'refresh-token',
     )
     .build();
 
